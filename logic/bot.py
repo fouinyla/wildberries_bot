2477460c1c6f177bd6977a0227abd7af31d114d0
Denis Bulychev <da.bulychev@yandex.ru>
2022-04-16 13:52:49 +0300

    Add start and query menu.

--- logic/bot.py
+++ logic/bot.py
@@ -14,8 +14,9 @@ Bot.set_current(bot)
 dp = Dispatcher(bot, storage=MemoryStorage())
 c = Controller(bot=bot)
 
-
+# это меню старт
 @dp.message_handler(commands='start')
+@dp.message_handler(Text(equals='Назад в главное меню'))
 async def command_start_process(message: types.Message):
     response = await c.command_start(message=message)
     await message.reply(
@@ -25,6 +26,19 @@ async def command_start_process(message: types.Message):
         reply=False
     )
 
+# это меню "поисковой запрос"
+@dp.message_handler(Text(equals='Поисковой запрос'))
+async def search_query_process(message: types.Message):
+    response = await c.search_query()
+    await message.reply(
+        text=response["text"],
+        reply_markup=response["markup"],
+        parse_mode="HTML",
+        reply=False
+    )
+
+
+'''
 @dp.message_handler(Text(equals="Notification")) 
 async def message_main_menu_button_notification_process(message: types.Message):
     response = await c.message_main_menu_buttons_click(message=message)
@@ -42,3 +56,4 @@ async def message_main_menu_buttons_click_process(message: types.Message):
         parse_mode="HTML",
         reply=False
     )
+'''
