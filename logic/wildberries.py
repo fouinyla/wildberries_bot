be79c33d9593d9fece9638a5960073867826b6f6
zhuchkov-artem <artem-1102@mail.ru>
2022-04-17 14:56:46 +0300

    request step validation

--- logic/wildberries.py
+++ logic/wildberries.py
@@ -1,5 +1,6 @@
 import httpx
 import json
+from typing import Union
 
 
 def get_response(url: str) -> httpx.Response:
@@ -14,15 +15,21 @@ def generate_url(query: str, gender: str = 'common', locale: str = 'ru',
 
 
 def get_hints_from_wb(query: str, gender: str = 'common', locale: str = 'ru',
-                      lang: str = 'ru') -> list[str]:
+                      lang: str = 'ru') -> Union[list[str], None]:
     url = generate_url(query, gender, locale, lang)
     response = get_response(url)
+    if response.status_code == 204:
+        return None
     json_array = json.loads(response.text)
     hints = [obj['name'] for obj in json_array if obj['type'] == 'suggest']
     return hints
 
 
 if __name__ == '__main__':
-    print(get_hints_from_wb('свитер'))  # одно слово
-    print(get_hints_from_wb('свитер женский оверсайз с шерстью'))  # конечный запрос
-    print(get_hints_from_wb(''))  # пустой запрос
+    print(get_hints_from_wb(''))  # пустой запрос -> [..., ...]
+    print(get_hints_from_wb('свитер'))  # одно слово -> [..., ...]
+    print(get_hints_from_wb('свитер женский оверсайз с шерстью'))  # конечный запрос -> []
+
+    print(get_hints_from_wb('?><+_'))  # -> None
+    print(get_hints_from_wb('abacab'))  # -> None
+    print(get_hints_from_wb('ббавмбб'))  # -> None
