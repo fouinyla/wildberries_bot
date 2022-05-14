84cd10f2a4370379d076eccb6c8de82c23a8f47b
zhuchkov-artem <artem-1102@mail.ru>
2022-05-14 14:05:24 +0300

    fix description

--- logic/telegram.py
+++ logic/telegram.py
@@ -5,7 +5,7 @@ CUSTOMER_CHANNEL_ID = '@opt_tyrke'
 
 async def subscribed(bot: aiogram.Bot, user_id: int) -> bool:
     """
-    Checks if the user is subscribed to the customer's channel.
+    Checks if user is subscribed to the customer's channel.
     """
     status = await bot.get_chat_member(chat_id=CUSTOMER_CHANNEL_ID,
                                        user_id=user_id)
