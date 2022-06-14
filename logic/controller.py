4b6fc50de32dd31dce8690d8098404510f55d012
Denis Bulychev <denis_bulychev@epam.com>
2022-06-14 23:33:52 +0300

    row 531 in controller.py - check for correct response

--- logic/controller.py
+++ logic/controller.py
@@ -528,7 +528,7 @@ class Controller:
     async def creating_queries_table(self, message, state):
         async with state.proxy() as data:
             card_data = await mpstats.get_card_data(data['article_for_queries'])
-            if card_data:
+            if card_data and type(card_data) == dict and 'words' in card_data.keys() and 'days' in card_data.keys():
                 result = utils.create_queries_table(card_data, data['article_for_queries'])
             else:
                 result = None
