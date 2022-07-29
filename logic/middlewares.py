aa330ee48b82598ff5b8e36d5ab03ea43cadd08e
zhuchkov-artem <artem-1102@mail.ru>
2022-07-29 15:57:48 +0300

    delete 'here' from middleware

--- logic/middlewares.py
+++ logic/middlewares.py
@@ -5,6 +5,5 @@ from aiogram.dispatcher.middlewares import BaseMiddleware
 
 class LoggingMiddleware(BaseMiddleware):
     async def on_pre_process_message(self, message: types.Message, data: dict):
-        print("here")
         logging.info("======= INCOMING UPDATE ========")
         logging.info(message)
