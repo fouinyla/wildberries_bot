3dbb8bf021f32c23ac680916fce25807fbe14003
Artem Zhuchkov <artem_zuchkov@epam.com>
2022-10-02 14:44:00 +0300

    changed mpstats account to eugeniy.bogdanov

--- const/const.py
+++ const/const.py
@@ -7,33 +7,20 @@ MPSTATS_TRENDS_URL = 'https://mpstats.io/api/wb/get/category/trends'
 MPSTATS_PRICE_SEGMENTATION_URL = 'https://mpstats.io/api/wb/get/category/price_segmentation'
 MPSTATS_SALES_URL = 'https://mpstats.io/api/wb/get/item/ARTICLE/by_keywords'
 
-COOKIES_PART = '_ym_uid=1661966793670747865; _ym_d=1661966793; ' \
-               'roistat_visit=3290381; roistat_first_visit=3290381; ' \
-               'roistat_visit_cookie_expire=1209600; roistat_is_need_' \
-               'listen_requests=0; roistat_is_save_data_in_cookie=1; ' \
-               '_ym_visorc=w; _ga=GA1.1.742800538.1661966796; _ym_isad=2; ' \
-               'roistat_marker=seo_google_; roistat_marker_old=seo_google_; ' \
-               'tmr_lvid=6289c8bade3f9a242ed71718c0ffb145; tmr_lvidTS=166196' \
-               '6798339; supportOnlineTalkID=E5Ekhb69DpsUBw2k1THQRuh20M' \
-               'carqLu; ___dc=3eb4747f-22a2-4cf1-8fb1-f8a29c16fa56; ' \
-               '_mpsuid=05ee9732e94ec88fc2843347605a75f6; mindboxDevi' \
-               'ceUUID=efb1f15e-ed26-4a59-98e2-3c2819b7b3f7; directCrm' \
-               '-session=%7B%22deviceGuid%22%3A%22efb1f15e-ed26-4a59' \
-               '-98e2-3c2819b7b3f7%22%7D; roistat_call_tracking=0; ' \
-               'roistat_emailtracking_email=null; roistat_emailtracking' \
-               '_tracking_email=null; roistat_emailtracking_emails=null; ' \
-               'roistat_cookies_to_resave=roistat_ab%2Croistat_ab_submit' \
-               '%2Croistat_visit%2Croistat_marker%2Croistat_marker_old%2' \
-               'Croistat_call_tracking%2Croistat_emailtracking_email%2Cr' \
-               'oistat_emailtracking_tracking_email%2Croistat_emailtrack' \
-               'ing_emails; acceptcookies=accepted; _ym_hostIndex=0-8%2C1' \
-               '-5; userlogin=a%3A2%3A%7Bs%3A3%3A%22lgn%22%3Bs%3A22%3A%22' \
-               'andriikudaev%40yandex.ru%22%3Bs%3A3%3A%22pwd%22%3Bs%3A32%' \
-               '3A%22aa261830dd81a1d4507e17ec188ee5eb%22%3B%7D; tmr_dete' \
-               'ct=1%7C1661968712348; _gr_session=%7B%22s_id%22%3A%22e0f' \
-               'da64a-3fc9-4b2e-912f-352ff6894349%22%2C%22s_time%22%3A166' \
-               '1968713198%7D; _ga_8S8Y1X62NG=GS1.1.1661966796.1.1.166196' \
-               '8713.0.0.0; tmr_reqNum=117'
+COOKIES_PART = 'tmr_reqNum=21; tmr_detect=0%7C1664710499716; supportOnlineTalkID=CLBFGobV' \
+               'EM8St3UaTzzGTxcOvpAqcAQD; _comagic_id5brIg=5703612438.8569687065.1664710369' \
+               '; _ga=GA1.1.820842487.1664644592; _ga_8S8Y1X62NG=GS1.1.1664710362.3.1.16647' \
+               '10497.0.0.0; _ym_visorc=b; popmechanic_sbjs_migrations=popmechanic_14184743' \
+               '75998%3D1%7C%7C%7C1471519752600%3D1%7C%7C%7C1471519752605%3D1; tmr_lvid=3f5' \
+               'a0166ad465f9bd1d7b40209b72030; tmr_lvidTS=1664644592457; _gr_session=%7B%22s' \
+               '_id%22%3A%22e41f91d4-52d3-439c-a1de-7f8ccabbcf1c%22%2C%22s_time%22%3A1664710' \
+               '497262%7D; _mpsuid=21197ff06ab8f2c1a424e9d26d0115ac; userlogin=a%3A2%3A%7Bs%3' \
+               'A3%3A%22lgn%22%3Bs%3A29%3A%22ip.evgeniy.bogdanov%40gmail.com%22%3Bs%3A3%3A%22' \
+               'pwd%22%3Bs%3A32%3A%2299fa197de27558f79782ea8373471beb%22%3B%7D; _cmg_csst5brI' \
+               'g=1664710365; __lhash_=8100f13f404b5dfe21de0380953110d2; _ym_isad=2; _ym_d=16' \
+               '64644591; _ym_uid=1664644591509828735; directCrm-session=%7B%22deviceGuid%22%' \
+               '3A%2293cc2701-545a-429b-a612-7a65f49c1124%22%7D; mindboxDeviceUUID=93cc2701-54' \
+               '5a-429b-a612-7a65f49c1124; mpgd=1664644585.60086208.59765316'
 
 MPSTATS_TRENDS = {'Число продаж': 'sales',
                   'Суммарная выручка в рубрике': 'revenue',
