baabd93e73bfa106a8ce894a5426803f5615fb53
fouinyla <mefouiny@gmail.com>
2022-10-07 19:55:56 +0300

    add alembic

--- db/models.py
+++ db/models.py
@@ -8,39 +8,39 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    email = Column(String, nullable=False)
-    phone_number = Column(String, nullable=False)
+    name = Column(String(100), nullable=False)
+    email = Column(String(100), nullable=False)
+    phone_number = Column(String(30), nullable=False)
     tg_id = Column(BigInteger, nullable=False)
-    tg_nickname = Column(String, nullable=False)
+    tg_nickname = Column(String(50), nullable=False)
     is_admin = Column(Boolean, nullable=False, default=False)
 
 
 class Query(Base):
     __tablename__ = "queries"
     id = Column(Integer, primary_key=True)
-    search_query = Column(String)
+    search_query = Column(String(100))
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
 
 
 class SEOquery(Base):
     __tablename__ = "SEO_queries"
     id = Column(Integer, primary_key=True)
-    query_for_SEO = Column(String)
+    query_for_SEO = Column(String(200))
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
 
 
 class SearchPosition(Base):
     __tablename__ = "search_position"
     id = Column(Integer, primary_key=True)
-    search_position_query = Column(String)
+    search_position_query = Column(String(200))
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
 
 
 class PriceSegmentation(Base):
     __tablename__ = "price_segmentations"
     id = Column(Integer, primary_key=True)
-    query_for_price = Column(String)
+    query_for_price = Column(String(200))
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
 
     
